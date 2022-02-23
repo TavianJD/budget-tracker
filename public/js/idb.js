93c23f774d51b2ec26a9ed264a5dbe24d03acf9f
hatetavian <tavianjdawson123@gmail.com>
2022-02-23 11:54:45 -0600

    re did the service worker and idb budget wasn't persisting on heroku

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,11 +1,12 @@
-// variable for connection
+// create variable to hold db connection
 let db;
+// establish a connection to IndexedDB database 
+const request = indexedDB.open('budget_tracker', 1);
 
-const request = indexedDB.open('budget-tracker', 1);
-
+// this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
     const db = event.target.result;
-    db.createObjectStore('new_transaction', { autoIncrement: true});
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 // upon a successful 
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // global constants 
-const APP_PREFIX = 'budgettrack-';     
+const APP_PREFIX = 'budgettrack';     
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
@@ -9,26 +9,16 @@ const FILES_TO_CACHE = [
   '/js/index.js',
   '/js/idb.js',
   '/manifest.json',
-  '/css/styles.css'
+  '/css/styles.css',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'  
 ];
-
-// retrieving information from the cache 
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-      //   console.log(e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-      //   console.log(e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
-});
 
 // adding files to the precache so that the application can use the cache (installing service worker)
 self.addEventListener('install', function (e) {
@@ -59,3 +49,22 @@ self.addEventListener('activate', function (e) {
   })
   )
 });
+
+// retrieving information from the cache 
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+      //   console.log(e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+      //   console.log(e.request.url)
+        return fetch(e.request)
+      }
+    })
+  )
+});
+
